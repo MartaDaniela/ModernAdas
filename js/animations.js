0e2962a5060c8c8d82d4c04da6a061ee8b804a70
Marta Daniela <acostadanielaa2@gmail.com>
2022-12-13 17:03:35 -0800

    design changes

--- js/animations.js
+++ js/animations.js
@@ -1,16 +1,4 @@
 
-function update(e){
-    var x = e.clientX || e.touches[0].clientX
-    var y = e.clientY || e.touches[0].clientY
-  
-    document.getElementById("title-imgss").style.setProperty('--cursorX', x + 'px')
-    document.getElementById("title-imgss").style.setProperty('--cursorY', y + 'px')
-  }
-  
-    
-  document.getElementById("title").addEventListener('mousemove',update)
-  document.getElementById("title").addEventListener('touchmove',update)
-
 /*
 function cursorChange(id){
   if(document.getElementById(id)!= "title"){
@@ -73,3 +61,5 @@ function setCursorByID(id,cursorStyle) {
      if (elem.style) elem.style.cursor=cursorStyle;
     }
    }
+
+  
